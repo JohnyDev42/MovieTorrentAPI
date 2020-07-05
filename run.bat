58cb866b0d42a73ce7e44bccd95d36ec4b935052
Arun Kumar Sahu (Temp) <c_arunsa@qti.qualcomm.com>
2020-07-05 14:50:00 +0530

    .gitignore added

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 
 @echo off
-cmd /k "cd /d D:\Practice\MovieRater\venv\Scripts & activate & cd /d    D:\Practice\MovieRater\ & python manage.py runserver 192.168.1.7:8000"
+cmd /k "python manage.py runserver localhost:8000"
